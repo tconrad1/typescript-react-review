98f6d850574b76ede3402729d742f64514ae0650
tconrad1 <tconrad1@umd.edu>
2025-08-29 19:45:21 -0400

    seeking work change

--- src/About.tsx
+++ src/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
             <h1>About</h1>
             <p>My name is <b>Thomas Conrad</b> and I am a software developer, and sophomore at <a href='https://umd.edu/'> UMD</a>.
              I have experience with <b>React</b>, <b>JS/Typescript</b>, <b>Node.js</b>, <b>Java</b>, <b> C</b>,<b> Python</b>, and <b>Express.js</b>. 
-             I am currently looking for a summer internship and more chances to gain experience. </p>
+             I am currently looking for an internship, part time work, and more chances to gain experience. </p>
             
              <Contact  name="Thomas Conrad" email="thomaswconrad2004@gmail.com" linkedin={null} children = {<p>Feel free to reach out to me via email!</p>}  /> 
             <div className='personal'> <p>Here is some fun personal stuff </p> <Picture title="my cat" source= "chayka.jpg"  description="an image of my cat chayka in which she looks very suspicious of the viewer. " /></div>
