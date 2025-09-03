55659c85bb97aa86ee624279c5f0a2467ad3d8d7
Thomas Conrad <tconrad1@umd.edu>
2025-09-03 18:24:59 -0400

    home page tweak

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
         <div >
             <h1>Home</h1>
             <p>Welcome to my website! I made this to review <b>React</b> and <b>TypesScript</b> and to talk a bit about myself. </p>
+            <h3>Click on the links above (or to the right on mobile) to navigate the various pages. </h3>
         </div>
     );
 };
